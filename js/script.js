a344105b0b75a2fb6b3ea153e2a2cd8da235fc7d
Gabri1391 <chiarellogabriele@gmail.com>
2022-06-23 15:54:21 +0200

    push

--- js/script.js
+++ js/script.js
@@ -2,20 +2,25 @@
 // Stabilire che per i numeri divisibili per 3 in console.log verrà scritto "Fizz"
 // Stabilire che per i numeri divisibili per 5 in console.log verrà scritto "Buzz"
 // Stabilire che per i numeri divisibili sia per 3 che per 5 in console.log verrà scritto "FizzBuzz"
+// Creo una costante per prendere l'id dall'HTML
 
 
+let square = document.getElementById('square')
+let listOfNumbers = '<ul>'
 
-    for (let i = 1; i <=100; i++){
-        
-        if (i % 3 === 0 && i % 5 === 0){
-            console.log('FizzBuzz');
-        }
-        else if (i % 3 === 0){
-            console.log ('Fizz');
-        }
-        else if (i % 5 === 0){
-            console.log('Buzz');
-        }
-        else console.log(i);
+for (let i = 1; i <=100; i++){
+
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log('FizzBuzz');
     }
+    else if (i % 3 === 0){
+        console.log ('Fizz');
+    }
+    else if (i % 5 === 0){
+        console.log('Buzz');
+    }
+    else console.log(i);
+
+    square.innerText += ` ${i}`;
+}
 
