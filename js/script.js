4c0513316b2c6591ba21310e2592da2e42a73af1
Gabri1391 <chiarellogabriele@gmail.com>
2022-06-23 16:09:51 +0200

    fine esercizio base

--- js/script.js
+++ js/script.js
@@ -5,11 +5,8 @@
 // Creo una costante per prendere l'id dall'HTML
 
 
-let square = document.getElementById('square')
-let listOfNumbers = '<ul>'
 
 for (let i = 1; i <=100; i++){
-
     if (i % 3 === 0 && i % 5 === 0){
         console.log('FizzBuzz');
     }
@@ -20,7 +17,5 @@ for (let i = 1; i <=100; i++){
         console.log('Buzz');
     }
     else console.log(i);
-
-    square.innerText += ` ${i}`;
 }
 
